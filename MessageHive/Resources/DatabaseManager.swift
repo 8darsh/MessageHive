47b4857cac2f2dd83afadc6ee2d0e79aba9715d6
Adarsh <86889416+8darsh@users.noreply.github.com>
2023-10-22 13:03:33 +0530

    handle auth session, add database

---
+++ MessageHive/Resources/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  MessageHive
+//
+//  Created by Adarsh Singh on 22/10/23.
+//
+
+import Foundation
--- Pods/Target Support Files/Pods-MessageHive/Pods-MessageHive-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MessageHive : NSObject
-@end
-@implementation PodsDummy_Pods_MessageHive
-@end
